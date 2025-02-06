4f2fb415421bf2550733d277ed32ff571e63ff5b
Peter Tessier <thepetertessier@gmail.com>
2025-02-06 15:23:02 -0500

    first commit

---
+++ scripts/player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Animation
+	if direction > 0:
+		animated_sprite_2d.play("run")
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.play("run")
+		animated_sprite_2d.flip_h = true
+	else:
+		animated_sprite_2d.play("idle")
+	
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
